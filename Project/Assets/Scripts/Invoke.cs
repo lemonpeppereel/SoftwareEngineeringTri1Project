5c3220c1276e2f825cc790448ae82eb7ea28e1f4
lieman27 <lieman27@bergen.org>
2025-12-03 11:34:23 -0500

    fixed the spawnpoint position, but need to make sure it spawns an actual weapon

--- Project/Assets/Scripts/Invoke.cs
+++ Project/Assets/Scripts/Invoke.cs
@@ -3,18 +3,19 @@ using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
-
+    public Transform spawnpoint;
     public GameObject testPrefab;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        spawnpoint = GameObject.FindGameObjectWithTag("Spawnpoint").transform;
         Invoke("Spawn", 1f);
     }
 
     public void Spawn()
     {
-        Instantiate(testPrefab);
+        Instantiate(testPrefab, spawnpoint.position, spawnpoint.rotation);
     }
 
 }

8ff5d164ebabeb57195f47dd3a8c3f9ec2c798e0
lieman27 <lieman27@bergen.org>
2026-03-11 23:56:41 -0400

    Readded the deathcountersingleton for clarity

--- Project/Assets/Scripts/IBall.cs
+++ Project/Assets/Scripts/IBall.cs
@@ -2,18 +2,18 @@ using UnityEngine;
 
 public interface IBall
 {
-    Rigidbody2D Rb { get; }
-    Vector2 Direction { get; }
-    float RotationSpeed { get; }
-    bool ForwardRotation { get; }
-    float Speed { get; }
-    int Damage { get; }
-    string TargetTag { get; }
-    string WeaponTag { get; }
-    SpriteRenderer WeaponSpriteRenderer { get; }
+    // Rigidbody2D Rb { get; }
+    // Vector2 Direction { get; }
+    // float RotationSpeed { get; }
+    // bool ForwardRotation { get; }
+    // float Speed { get; }
+    // int Damage { get; }
+    // string TargetTag { get; }
+    // string WeaponTag { get; }
+    // SpriteRenderer WeaponSpriteRenderer { get; }
 
-    private void Rotate();
-    private void WeaponHit(Collision2D collision);
-    private void BallHit(Collision2D collision);
+    // private void Rotate();
+    // private void WeaponHit(Collision2D collision);
+    // private void BallHit(Collision2D collision);
 
 }
--- Project/Assets/Tests/Editor/BallTests.cs
+++ Project/Assets/Tests/Editor/BallTests.cs
@@ -2,50 +2,50 @@ using UnityEngine;
 
 public class BallTests
 {
-    private class MockBall : IBall
-    {
-        Rigidbody2D Rb { get; }
-        Vector2 Direction { get; }
-        float RotationSpeed { get; set; } = 360f;
-        bool ForwardRotation { get; set; } = true;
-        float Speed { get; }
-        int Damage { get; }
-        string TargetTag { get; }
-        string WeaponTag { get; }
-        SpriteRenderer WeaponSpriteRenderer { get; }
+    // private class MockBall : IBall
+    // {
+    //     Rigidbody2D Rb { get; }
+    //     Vector2 Direction { get; }
+    //     float RotationSpeed { get; set; } = 360f;
+    //     bool ForwardRotation { get; set; } = true;
+    //     float Speed { get; }
+    //     int Damage { get; }
+    //     string TargetTag { get; }
+    //     string WeaponTag { get; }
+    //     SpriteRenderer WeaponSpriteRenderer { get; }
 
-        private void Rotate();
-        private void WeaponHit(Collision2D collision);
-        private void BallHit(Collision2D collision);
-    }
+    //     private void Rotate();
+    //     private void WeaponHit(Collision2D collision);
+    //     private void BallHit(Collision2D collision);
+    // }
 
-    [Test]
-    public void Rotate_ForwardRotation()
-    {
-        // Arrange
-        var mockBall = new MockBall();
-        IBall ball = mockBall;
-        ball.ForwardRotation = true;
+    // [Test]
+    // public void Rotate_ForwardRotation()
+    // {
+    //     // Arrange
+    //     var mockBall = new MockBall();
+    //     IBall ball = mockBall;
+    //     ball.ForwardRotation = true;
 
-        // Act
-        ball.Rotate();
+    //     // Act
+    //     ball.Rotate();
 
-        // Assert
-        Assert.AreEqual(transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime)); // something needed here to compare to
-    }
+    //     // Assert
+    //     Assert.AreEqual(transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime)); // something needed here to compare to
+    // }
 
-    [Test]
-    public void Rotate_BackwardRotation()
-    {
-        // Arrange
-        var mockBall = new MockBall();
-        IBall ball = mockBall;
-        ball.ForwardRotation = true;
+    // [Test]
+    // public void Rotate_BackwardRotation()
+    // {
+    //     // Arrange
+    //     var mockBall = new MockBall();
+    //     IBall ball = mockBall;
+    //     ball.ForwardRotation = true;
 
-        // Act
-        ball.Rotate();
+    //     // Act
+    //     ball.Rotate();
 
-        // Assert
-        Assert.AreEqual(transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime)); // something needed here to compare with
-    }
+    //     // Assert
+    //     Assert.AreEqual(transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime)); // something needed here to compare with
+    // }
 }
